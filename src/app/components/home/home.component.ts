b21a5d9d85c3db96accb5f3e3b395e8cb4ddaaaf
Iván <72264031+xIvqn@users.noreply.github.com>
2024-04-18 09:51:28 +0200

    wip: home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GameLogicService } from 'src/app/services/game-logic.service';
+import { ThemeService } from 'src/app/services/theme.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  roomForm = new FormGroup({
+    board: new FormControl('Choose an option')
+  });
+  
+  constructor(private themeService: ThemeService, private gameLogicService: GameLogicService, private router: Router) {
+  }
+
+  getAvailableBoards(): string[] {
+    return this.gameLogicService.getAvailableBoards();
+  }
+
+  startRoom() {
+    console.log(this.roomForm.get('board')?.value());
+    
+    // this.router.navigate(['/room']);
+  }
+
+  swapTheme() {
+    this.themeService.swapTheme();
+  }
+
+}
